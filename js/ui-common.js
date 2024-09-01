f8f9dcbc80d66a4faa78086af12ca802bcf922a4
Gom0323 <sejong0323@gmail.com>
2024-09-01 19:45:50 +0900

    slide부분 거의 완성하였고 정지 시작버튼 및 페이지수 만들어야함

--- js/ui-common.js
+++ js/ui-common.js
@@ -6,4 +6,38 @@ $(function () {
     .on("mouseleave", function () {
       $("#header").removeClass("on");
     });
+
+    const swiper = new Swiper('.swiper', {
+      autoplay: {
+        delay: 3000,
+      },
+      loop: false,
+      pagination: {
+        el: '.swiper-pagination',
+      },
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+      on: {
+        slideChange: function () {
+          changeBackgroundColor(this.activeIndex);
+        }
+      },
+     });
+
+    function changeBackgroundColor(index) {
+      const container = document.getElementById('mainSlide');
+      const colors = [
+        '#92b5f5',
+        '#c9b980',
+        '#88c7ec',
+        '#d3b290',
+        '#efb4ac',
+        '#67aadf',
+        '#70c9f0'
+      ];
+
+      container.style.backgroundColor = colors[index] || '#fff'
+    }
 });
