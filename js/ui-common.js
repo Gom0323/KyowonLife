b0d6c8cc02fbed40918cc7eb4f401a0380859e95
Gom0323 <sejong0323@gmail.com>
2024-09-02 20:18:11 +0900

    슬라이더부분 완성

--- js/ui-common.js
+++ js/ui-common.js
@@ -7,37 +7,72 @@ $(function () {
       $("#header").removeClass("on");
     });
 
-    const swiper = new Swiper('.swiper', {
-      autoplay: {
-        delay: 3000,
+  const swiper = new Swiper(".swiper", {
+    autoplay: {
+      disableOnInteraction: false,
+      delay: 3000,
+    },
+    loop: false,
+    pagination: {
+      el: ".swiper-pagination",
+      type: "fraction",
+      formatFractionCurrent: function (number) {
+        return number < 7 ? "" + number : number;
       },
-      loop: false,
-      pagination: {
-        el: '.swiper-pagination',
+      formatFractionbetween: function (string) {
+        return string;
       },
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
+      formatFractionTotal: function (number) {
+        return number < 7 ? "" + number : number;
       },
-      on: {
-        slideChange: function () {
-          changeBackgroundColor(this.activeIndex);
-        }
+      renderFraction: function (currentClass, totalClass) {
+        return (
+          '<span class="' +
+          currentClass +
+          '"></span>' +
+          "<span> / </spna>" +
+          '<span class="' +
+          totalClass +
+          '"></span>'
+        );
       },
-     });
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    on: {
+      slideChange: function () {
+        changeBackgroundColor(this.activeIndex);
+      },
+    },
+  });
 
-    function changeBackgroundColor(index) {
-      const container = document.getElementById('mainSlide');
-      const colors = [
-        '#92b5f5',
-        '#c9b980',
-        '#88c7ec',
-        '#d3b290',
-        '#efb4ac',
-        '#67aadf',
-        '#70c9f0'
-      ];
-
-      container.style.backgroundColor = colors[index] || '#fff'
+  let stopPlay = false;
+  $(".btn_wrap .toggle_btn").on("click", function () {
+    if (stopPlay) {
+      $(this).find("i").attr("class", "ri-pause-fill");
+      swiper.autoplay.start();
+      stopPlay = false;
+    } else {
+      $(this).find("i").attr("class", "ri-play-fill");
+      swiper.autoplay.stop();
+      stopPlay = true;
     }
+  });
+
+  function changeBackgroundColor(index) {
+    const container = document.getElementById("mainSlide");
+    const colors = [
+      "#92b5f5",
+      "#c9b980",
+      "#88c7ec",
+      "#d3b290",
+      "#efb4ac",
+      "#67aadf",
+      "#70c9f0",
+    ];
+
+    container.style.backgroundColor = colors[index] || "#fff";
+  }
 });
